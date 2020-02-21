1200b4290e8383cfa6cd9aa3fb77be217b603e4d
hanny242 <56655159+hanny242@users.noreply.github.com>
2020-02-21 11:50:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use("/", authRouter);
-app.use("/index", indexRouter);
+app.use("/home", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
